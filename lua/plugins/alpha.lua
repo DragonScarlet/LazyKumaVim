82bfa6a2d25b79cc66414cd8cbad8be1c56d9460
Dimitri Wulffert <dimitri.wulffert.sp@lhind.dlh.de>
2023-08-03 15:35:12 +0200

    Make alpha cool

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -49,6 +49,15 @@ return {
         -- Send config to alpha
         alpha.setup(dashboard.opts)
 
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "LazyVimStarted",
+            callback = function()
+                local stats = require("lazy").stats()
+                local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+                dashboard.section.footer.val = "熊ビム loaded " .. stats.count .. " plugins in " .. ms .. "ms 速い!" 
+                pcall(vim.cmd.AlphaRedraw)
+            end,
+        })
         -- Disable folding on alpha buffer
         vim.cmd([[
     autocmd FileType alpha setlocal nofoldenable
