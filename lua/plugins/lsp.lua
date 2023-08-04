86ab5e83d59ebeaa56d388c305977d3eac78abfa
Dimitri Wulffert <dimitri.wulffert.sp@lhind.dlh.de>
2023-08-04 10:20:57 +0200

    Add diagnostics

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,21 +2,21 @@ return {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v1.x',
     dependencies = {
-        { 'neovim/nvim-lspconfig' },                 -- Required
-        { 'williamboman/mason.nvim' },               -- Optional
-        { 'williamboman/mason-lspconfig.nvim' },     -- Optional
+        { 'neovim/nvim-lspconfig' },             -- Required
+        { 'williamboman/mason.nvim' },           -- Optional
+        { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
         -- Autocompletion
-        { 'hrsh7th/nvim-cmp' },             -- Required
-        { 'hrsh7th/cmp-nvim-lsp' },         -- Required
-        { 'hrsh7th/cmp-buffer' },           -- Optional
-        { 'hrsh7th/cmp-path' },             -- Optional
-        { 'saadparwaiz1/cmp_luasnip' },     -- Optional
-        { 'hrsh7th/cmp-nvim-lua' },         -- Optional
+        { 'hrsh7th/nvim-cmp' },         -- Required
+        { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+        { 'hrsh7th/cmp-buffer' },       -- Optional
+        { 'hrsh7th/cmp-path' },         -- Optional
+        { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+        { 'hrsh7th/cmp-nvim-lua' },     -- Optional
 
         -- Snippets
-        { 'L3MON4D3/LuaSnip' },                 -- Required
-        { 'rafamadriz/friendly-snippets' },     -- Optional
+        { 'L3MON4D3/LuaSnip' },             -- Required
+        { 'rafamadriz/friendly-snippets' }, -- Optional
     },
     config = function()
         local lsp = require('lsp-zero')
@@ -30,5 +30,14 @@ return {
         lsp.nvim_workspace()
 
         lsp.setup()
+
+        vim.diagnostic.config({
+            virtual_text = true,
+            signs = true,
+            update_in_insert = false,
+            underline = true,
+            severity_sort = false,
+            float = true,
+        })
     end
 }
