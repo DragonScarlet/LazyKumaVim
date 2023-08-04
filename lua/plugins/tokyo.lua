f2764e0da1b42902275224b68731ba1eecc7b35e
Dimitri Wulffert <dimknight@gmail.com>
2023-08-04 10:41:00 +0200

    Clean warnings

--- lua/plugins/tokyo.lua
+++ lua/plugins/tokyo.lua
@@ -24,16 +24,9 @@ return {
             hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
             dim_inactive = false,     -- dims inactive windows
             lualine_bold = false,     -- When `true`, section headers in the lualine theme will be bold
-            --- You can override specific color groups to use other groups or a hex color
-            --- function will be called with a ColorScheme table
-            ---@param colors ColorScheme
             on_colors = function(colors)
                 colors.fg = "#f0c0f5"
             end,
-            --- You can override specific highlights to use other groups or a hex color
-            --- function will be called with a Highlights and ColorScheme table
-            ---@param highlights Highlights
-            ---@param colors ColorScheme
 
             on_highlights = function(hl, c)
                 local prompt = "#2d3149"
