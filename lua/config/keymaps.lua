2a2bdded6046499436e11c8dec10406fc7545953
Dimitri Wulffert <dimitri.wulffert.sp@lhind.dlh.de>
2023-10-03 14:57:08 +0200

    Add descriptions

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,22 +2,25 @@ local keymap = vim.keymap.set
 local command = vim.api.nvim_create_user_command
 
 vim.g.mapleader = " "
-keymap("n", "<leader>pv", vim.cmd.Ex)
 
 -- Exit
 command('Qq', 'q! | q! | q!', {})
 
 -- Autoformat
-keymap("v", "<leader>f", ":'<, '> lua vim.lsp.buf.format() <CR>")
-keymap("n", "<leader>ff", ":lua vim.lsp.buf.format() <CR>")
+keymap("v", "<leader>f", ":'<, '> lua vim.lsp.buf.format() <CR>", { desc = "Format selection" })
+keymap("n", "<leader>ff", ":lua vim.lsp.buf.format() <CR>", { desc = "File Format" })
 
 
 -- nvim-tree
-keymap("n", "<leader>pt", "<cmd>NvimTreeToggle<CR>")
+keymap("n", "<leader>pt", "<cmd>NvimTreeToggle<CR>", { desc = "Project Tree" })
 
 -- Git
-keymap("n", "<leader>gs", vim.cmd.Git)
+keymap("n", "<leader>gs", vim.cmd.Git, { desc = "Git Status" })
+keymap('n', '<leader>gr', ":2,$s/pick/s<CR>", { desc = "Git Replace (Pick with S)" })
+
+
+
 
 -- Dap
-keymap("n", "<leader>df", "<Cmd>lua require'jdtls'.test_class()<CR>")
-keymap("n", "<leader>dn", "<Cmd>lua require'jdtls'.test_nearest_method()<CR>")
+keymap("n", "<leader>tc", "<Cmd>lua require'jdtls'.test_class()<CR>", { desc = "Test Class" })
+keymap("n", "<leader>tf", "<Cmd>lua require'jdtls'.test_nearest_method()<CR>", { desc = "Test Function" })
